c57680c9deb4be1918d9a59f80f5c2ff97cd9bc3
unknown <Administrator@WIMMER.usask.ca>
2020-06-22 17:23:27 -0600

    compatible with main sh

--- src/ssuis_cpsk_SNP_calling.sh
+++ src/ssuis_cpsk_SNP_calling.sh
@@ -4,12 +4,15 @@ usage(){
   echo "
 Usage $0
   Arguments:
-  -i|--input  input fastq file
-  -o|--outdir output directory
-  -h|--help   display help message
+  -i|--input    input fastq file
+  -o|--outdir   output directory
+  -t|--threads  threads
+  -h|--help     display help message
 "
 
 }
+
+n_threads=4
 
 # parse arguments
 if [ $# == 0 ]
@@ -18,26 +21,28 @@ then
   exit 0
 fi
 
-opts=`getopt -o hi:o: -l input:,outdir:,help -- "$@"` 
+opts=`getopt -o hi:o:t: -l input:,outdir:,threads:,help -- "$@"` 
 eval set -- "$opts"
 
 while true; do
   case "$1" in
     -i|--input) in_file=$2; shift 2 ;;
     -o|--outdir) out_dir=$2; shift 2 ;;
+    -t|--threads) n_threads=$2; shift 2 ;;
     --) shift; break ;;
     -h|--help) usage; exit ;;
   esac
 done
 
+# get script dir
+Script_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # initialize variables
 filename=$(echo $(basename ${in_file%.*}))
-ref_path="/home/arnie/JimmyFiles/SsuisSerotyping_pipeline/Ssuis_cps2K.fasta"
+ref_path=$(echo $(dirname $Script_DIR)/database/Ssuis_cps2K.fasta)
 
 # main
 main() {
-  # activate required conda enviornment
-  source /opt/miniconda2/bin/activate minimap2-2.17
 
   # create outdir
   mkdir -p $out_dir
@@ -45,13 +50,11 @@ main() {
   echo "Analyzing: $(echo $(basename $in_file))..."
 
   # run alignment
-  NanoFilt -q 5 < $in_file | \
-  minimap2 -ax map-ont -t 32 $ref_path - > $out_dir/$filename.sam
+  minimap2 -ax map-ont -t $n_threads $ref_path $in_file > $out_dir/$filename.sam
   
   # variant calling
-  source /opt/miniconda2/bin/activate freebayes-1.3.2
   samtools faidx $ref_path
-  samtools view -bS -@ 32 $out_dir/$filename.sam > $out_dir/$filename.bam
+  samtools view -bS -@ $n_threads $out_dir/$filename.sam > $out_dir/$filename.bam
   samtools sort $out_dir/$filename.bam > $out_dir/$filename.sorted.bam
   freebayes -p 1 -f $ref_path $out_dir/$filename.sorted.bam | vcffilter -f "QUAL > 20" > $out_dir/$filename.vcf
   
@@ -59,7 +62,7 @@ main() {
   #bcftools view --threads 16 -v snps $out_dir/$filename.vcf > $out_dir/$filename.raw.vcf
   #vcfutils.pl varFilter -Q 20 $out_dir/$filename.raw.vcf > $out_dir/$filename.vcf
 
-  # clean up tmp/
+  # clean up tmp files
   rm $(echo $(dirname $ref_path)/*.fai)
   rm $out_dir/*.bam
   rm $out_dir/*.sam
